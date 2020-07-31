5f7b064a0fe36c9113eb713aa795a192310f461d
Hélio Shinohara <strokestyle.shinohara@gmail.com>
2020-07-31 01:04:47 -0300

    Atualização com as features da Aula 03.

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,29 +1,162 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
 import { Link } from 'react-router-dom';
-
+import FormField from '../../../components/FormField';
 
 function CadastroCategoria() {
-    return (
-      <PageDefault>
-        <h1>Página de Cadastro de Categoria</h1>
+  const [categorias, setCategorias] = useState([]);
+  
+  /* const valoresIniciais = {
+    nome: 'Categoria Inicial',
+    descricao: 'Descrição inicial',
+    cor: '#000000',
+  } */
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: '',
+  }
+  const [values, setValues] = useState(valoresIniciais);
+  
+  //const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');
 
-        <form>
-          <label>
-            Nome da Categoria:
-            <input type="text" />
-          </label>
-
-          <button>
-            Cadastrar
-          </button>
-        </form>
-
-        <Link to="/">
-          Ir para a Home
-        </Link>
-      </PageDefault>
-    )
+  // chave: pode ser nome, descricao, etc.
+  function setValue(chave, valor) {
+    setValues({
+      ...values,
+      [chave]: valor,
+    })
   }
 
-  export default CadastroCategoria;
+  function handleChange(infosDoEvento) {
+    //console.log('[nomeDaCategoria]', nomeDaCategoria);
+    //console.log('[infosDoEvento.target.value]', infosDoEvento.target.value);
+    //setNomeDaCategoria(infosDoEvento.target.value);
+    //setValue('nome', infosDoEvento.target.value);
+    
+    //setValue(infosDoEvento.target.getAttribute('name'), infosDoEvento.target.value);
+    
+    //As duas linhas abaixo estão dando erro ao tentar digitar nos campos de texto, não entendi o porque.
+    /*const {getAttribute, value} = infosDoEvento.target;
+    setValue(getAttribute('name'), value);*/
+    //As duas linhas abaixo resolveram o problema das duas linhas acima.
+    const {name, value} = infosDoEvento.target;
+    setValue(name, value);
+  }
+
+  return (
+    <PageDefault>
+      {/* <h1>Cadastro de Categoria: {nomeDaCategoria}</h1> */}
+      <h1>Cadastro de Categoria: {values.nome}</h1>
+
+      <form onSubmit={function handleSubmit(infosDoEvento) {
+        infosDoEvento.preventDefault();
+        //console.log('Você tentou enviar o form né?');
+        setCategorias([
+          // ...categorias, nomeDaCategoria
+          ...categorias, values
+        ]);
+
+        //setValues({});
+        setValues(valoresIniciais);
+      }}>
+        {/* State */}
+        {/*<div>
+          <label>
+            Nome da Categoria: 
+            <input type="text"
+            name="nome"
+            // value={nomeDaCategoria}
+            value={values.nome}
+            // onChange={(infosDoEvento) => {
+              //console.log('[nomeDaCategoria]', nomeDaCategoria);
+              //console.log('[infosDoEvento.target.value]', infosDoEvento.target.value);
+              //setNomeDaCategoria(infosDoEvento.target.value);
+              //setValue('nome', infosDoEvento.target.value);
+              setValue(infosDoEvento.target.getAttribute('name'), infosDoEvento.target.value);
+            }} //
+            onChange={handleChange}
+            />
+          </label>
+        </div>*/}
+        <FormField
+          label="Nome da Categoria"
+          type="text"
+          name="nome"
+          value={values.nome}
+          onChange={handleChange}
+        />
+
+        <FormField
+          label="Descrição"
+          type="comment"
+          name="descricao"
+          value={values.descricao}
+          onChange={handleChange}
+        />
+        {/*<div>
+          <label>
+            Descrição: 
+            <textarea type="text"
+            name="descricao"
+            // value={nomeDaCategoria}
+            value={values.descricao}
+            // onChange={(infosDoEvento) => {
+              //console.log('[nomeDaCategoria]', nomeDaCategoria);
+              //console.log('[infosDoEvento.target.value]', infosDoEvento.target.value);
+              //setNomeDaCategoria(infosDoEvento.target.value);
+              //setValue('descricao', infosDoEvento.target.value);
+            //}}
+            onChange={handleChange}
+            />
+          </label>
+        </div>*/}
+
+
+        <FormField
+          label="Cor"
+          type="color"
+          name="cor"
+          value={values.cor}
+          onChange={handleChange}        
+        />
+        {/*<div>
+          <label>
+            Cor: 
+            <input type="color"
+            name="cor"
+            // value={nomeDaCategoria}
+            value={values.cor}
+            // onChange={(infosDoEvento) => {
+              //console.log('[nomeDaCategoria]', nomeDaCategoria);
+              //console.log('[infosDoEvento.target.value]', infosDoEvento.target.value);
+              //setNomeDaCategoria(infosDoEvento.target.value);
+              //setValue('cor', infosDoEvento.target.value);
+            //}}
+            onChange={handleChange}
+            />
+          </label>
+        </div>*/}
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+        <ul>
+          {categorias.map((categoria, indice) => {
+            return (
+            <li key={`${categoria}${indice}`}>
+              {categoria.nome}
+            </li>
+            )
+          })}
+        </ul>
+
+      <Link to="/">
+        Ir para a Home
+      </Link>
+    </PageDefault>
+  )
+}
+
+export default CadastroCategoria;
